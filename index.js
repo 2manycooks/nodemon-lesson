a6ea9b31d95e65a59977433c2c5ff433d031eb5b
2manycooks <73556731+2manycooks@users.noreply.github.com>
2022-08-30 12:16:02 -0700

    demonstrating gitignore

---
+++ index.js
@@ -0,0 +1,17 @@
+// let i = 2
+
+const dayjs = require("dayjs");
+
+// let count = () => {
+//     console.log(i)
+//     i *= 2
+// }
+
+// let myTimer = setInterval(count, 2000)
+
+// let's use Day.js!
+// syntax for dayjs=
+// dayjs("day you want").format()
+
+console.log(dayjs().format("DD MM YYYY"))
+
